6b46836e71738f65ce7d48f88f1988e8a149042c
wxb <15730971619@163.com>
2020-03-30 21:31:11 +0800

    12

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -118,7 +118,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = initial * (1 + percent * 0.01) . pow(3)
+fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
 
 /**
  * Простая
